d05481e94e83c848e0efa3c3f65c0eab9af689e7
ajenningsfrankston <ajenningsfrankston@gmail.com>
2021-12-05 04:42:04 +0000

    removed double pkl

--- utils.py
+++ utils.py
@@ -11,12 +11,10 @@ def save_stuff(model,name):
         Path(MODEL_FOLDER).mkdir(exist_ok=True, parents=True)
     except Exception as ex:
         pass
-    name = name + ".pkl"
     pd.to_pickle(model, f"{MODEL_FOLDER}/{name}.pkl")
 
 
 def load_stuff(name):
-    name = name + ".pkl"
     path = Path(f"{MODEL_FOLDER}/{name}.pkl")
     if path.is_file():
         model = pd.read_pickle(f"{MODEL_FOLDER}/{name}.pkl")
